d17f648f7df549451b2c8ed93efa8d469a893284
agungdlgs <agungdwi.lambang@bukalapak.com>
2022-04-02 23:04:30 +0700

    user & msg constructor

---
+++ utils/users.js
@@ -0,0 +1,21 @@
+class UsersDB {
+  constructor() {
+    this.users = [];
+  }
+
+  addUser(user) {
+    this.users = [...this.users, user];
+  }
+
+  getUser(id) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  removeUser(id) {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+}
+
+module.exports = () => {
+  return new UsersDB();
+};
