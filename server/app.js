3d00807c7458ad89200670aa30cb25ffa4ac081f
agungdlgs <agungdwi.lambang@bukalapak.com>
2022-04-02 16:46:48 +0700

    setup nuxt use nodemon

--- server/app.js
+++ server/app.js
@@ -1,3 +1,12 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+
+io.on('connection' , (socket) => {
+  // socket io code here
+});
+
+module.exports = {
+  app,
+  server,
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const { app, server } = require('./app')
+const consola = require('consola');
+const { Nuxt, Builder } = require('nuxt');
+const { app, server } = require('./app');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+const config = require('../nuxt.config.js');
+config.dev = process.env.NODE_ENV !== 'production';
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+async function start() {
+  const nuxt = new Nuxt(config);
+
+  const { host, port } = nuxt.options.server;
+
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  } else {
+    await nuxt.ready();
+  }
+
+  app.use(nuxt.render);
+
+  server.listen(port, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true,
+    });
+  });
+}
+start();
