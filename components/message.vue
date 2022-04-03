025d837f1b8468a6c7c57b5e28a0ac4f16604d26
agungdlgs <agungdwi.lambang@bukalapak.com>
2022-04-03 15:08:42 +0700

    url type msg

--- components/message.vue
+++ components/message.vue
@@ -4,7 +4,18 @@
   </div>
   <div v-else :class="`flex flex-col mb-2 ${getClass.wrapper}`">
     <div :class="`w-9/12 py-2 px-4 rounded-lg break-words ${getClass.text}`">
-      {{ message.text }}
+      <a :href="message.text" 
+        target="_blank" 
+        v-if="isMsgUrl"
+        class="decoration-solid text-sky-600 decoration-sky-600"
+      >{{ message.text }}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 align-text-bottom inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg>
+      </a>
+      <template v-else>
+        {{ message.text }}
+      </template>
     </div>
     <div class="w-9/12 text-right">
       <span :class="`text-sm ${getClass.name}`">{{ message.name }}</span>
@@ -13,6 +24,7 @@
   </div>
 </template>
 <script>
+const urlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
 export default {
   name: 'message',
   props: {
@@ -28,6 +40,9 @@ export default {
     }
   },
   computed: {
+    isMsgUrl() {
+      return urlRegex.test(this.message.text);
+    },
     getClass() {
       return this.isMe ? { 
         wrapper: 'items-start',
