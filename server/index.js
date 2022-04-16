416a4aa487e5ad7960f8b392dd92159b5a524df7
hapdesmith <agungdlgs@gmail.com>
2022-04-16 12:24:38 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const consola = require('consola');
 const { Nuxt, Builder } = require('nuxt');
 const { app, server } = require('./app');
-// const express = require('express');
 
 const config = require('../nuxt.config.js');
 config.dev = process.env.NODE_ENV !== 'production';
-const PORT = process.env.PORT || 3000;
 
 async function start() {
   const nuxt = new Nuxt(config);
 
-  const { host } = nuxt.options.server;
+  const { host, port } = nuxt.options.server;
 
   if (config.dev) {
     const builder = new Builder(nuxt);
@@ -21,12 +19,10 @@ async function start() {
 
   app.use(nuxt.render);
 
-  server
-    .use((req, res) => res.sendFile('/', { root: __dirname }))
-    .listen(PORT, () => {
-      consola.ready({
-        message: `Server listening on http://${host}:${PORT}`,
-        badge: true,
+  server.listen(port, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true,
     });
   });
 }
